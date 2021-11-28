572ba77680f45dd682af47d5e0bddcf696ee38b6
robilie <obilie.roberta@gmail.com>
2021-11-27 16:37:12 -0800

    Added border, images and some styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Hello{" "}
+      <div className="background">
+        Hello
+        <div className="dictionary-box">
+          I will contain the dictionary information
+        </div>
+      </div>
       <footer className="footer">
         This project was coded by Roberta Obilie and is open-sourced on Github
         and hosted on Netlify
