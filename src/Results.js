1ffdaa2cd44d26071e158344bcab0d4cd47c4590
robilie <obilie.roberta@gmail.com>
2021-12-05 22:36:40 -0800

    displayed phonetics info

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 import "./Results.css";
 
 export default function Results(props) {
@@ -7,6 +8,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.data.word}</h2>
+        <Phonetics phonetics={props.data.phonetics} />
         {props.data.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
